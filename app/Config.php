0bffc65b4fbb9292d38d357468b70bc9bc736f7e
Jose Perea <josepereab@gmail.com>
2021-11-30 14:12:10 +0100

    segunda

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+    static public $mvc_bd_hostname = "localhost";
+    static public $mvc_bd_nombre   = "alimentos32021";
+    static public $mvc_bd_usuario  = "myjavaf6fdc";
+    static public $mvc_bd_clave    = "Ub42dvXt";
+    static public $mvc_vis_css     = "estilo.css";
+ }
+?>
